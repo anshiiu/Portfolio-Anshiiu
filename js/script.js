4e75c537af7c349f5641ba476fdee1a477dbb16d
anshiiu <anshi.sosa30@gmail.com>
2026-06-26 01:46:13 -0300

    Se subieron todos los archivos

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const filterContainer = document.querySelector(".gallery__categories");
+const galleryItems = document.querySelectorAll(".project__card");
+
+filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")) {
+        
+        filterContainer.querySelector(".active").classList.remove("active");
+        event.target.classList.add("active");
+        
+        const filterValue = event.target.getAttribute("data-filter");
+        
+        galleryItems.forEach(item => {
+            if (item.classList.contains(filterValue) || filterValue === "all") {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            } else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
+
+function scrollToSection(sectionId) {
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+        targetSection.scrollIntoView({ behavior: "smooth" });
+        
+        const menuCheckbox = document.getElementById("menu-toggle");
+        if (menuCheckbox && menuCheckbox.checked) {
+            menuCheckbox.checked = false;
+        }
+    }
+}
